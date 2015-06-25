6584206a0047171922d459c5f125be89f3a6c0cd
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2015-06-26 02:24:31 +0900

    Use more preferred modules for testing
    gulp-util -> vinyl
    es.wait -> concat-stream
    es.readArray -> from2-string

--- test/stream.js
+++ test/stream.js
@@ -1,27 +1,27 @@
 'use strict';
 
-var plugin = require('../lib/index');
+var plugin = require('..');
 var fs = require('fs');
 var path = require('path');
-var gutil = require('gulp-util');
+var File = require('vinyl');
 var assert = require('assert');
-var es = require('event-stream');
+var concatStream = require('concat-stream');
+var stringToStream = require('from2-string');
 
 function compare(fixture, expected, stream, done) {
-    var fakeFile = new gutil.File({
+    var fakeFile = new File({
+        base: path.resolve('pages'),
+        path: path.join('pages', 'index.html'),
         contents: fixture
     });
-
-    fakeFile.base = path.join(fakeFile.cwd, 'pages');
-    fakeFile.path = path.join(fakeFile.cwd, path.join('pages', 'index.html'));
 
     stream.write(fakeFile);
 
     stream.once('data', function (file) {
         assert(file.isStream());
 
-        file.contents.pipe(es.wait(function (err, data) {
-            assert.equal(data.toString(), expected.toString());
+        file.contents.pipe(concatStream({encoding: 'string'}, function (data) {
+            assert.equal(data, expected);
             done();
         }));
     });
@@ -30,7 +30,7 @@ function compare(fixture, expected, stream, done) {
 describe('Stream mode', function () {
     it('should replace blocks', function (done) {
         var fixture = fs.createReadStream(path.join('test', 'fixture.html'));
-        var expected = fs.readFileSync(path.join('test', 'expected.html'));
+        var expected = fs.readFileSync(path.join('test', 'expected.html'), 'utf8');
 
         var stream = plugin({
             css: 'css/combined.css',
@@ -78,100 +78,100 @@ describe('Stream mode', function () {
     });
 
     it('should work with inline html', function (done) {
-        var fixture = ['<!DOCTYPE html><head><!-- build:css --><link rel="stylesheet" href="_index.prefix.css"><!-- endbuild --></head>'];
+        var fixture = '<!DOCTYPE html><head><!-- build:css --><link rel="stylesheet" href="_index.prefix.css"><!-- endbuild --></head>';
         var expected = '<!DOCTYPE html><head><link rel="stylesheet" href="css/combined.css"></head>';
 
         var stream = plugin({css: 'css/combined.css'});
-        compare(es.readArray(fixture), expected, stream, done);
+        compare(stringToStream(fixture), expected, stream, done);
     });
 
     it('should not fail if there are no build tags at all', function (done) {
-        var fixture = ['<!DOCTYPE html><head><link rel="stylesheet" href="_index.prefix.css"></head>'];
+        var fixture = '<!DOCTYPE html><head><link rel="stylesheet" href="_index.prefix.css"></head>';
 
         var stream = plugin({css: 'css/combined.css'});
-        compare(es.readArray(fixture), fixture[0], stream, done);
+        compare(stringToStream(fixture), fixture, stream, done);
     });
 
     describe('Options', function () {
 
         describe('keepUnassigned', function () {
             it('Should keep empty blocks', function (done) {
-                var fixture = ['<html>\n<!-- build:js -->\nSome text\n<!-- endbuild -->\n</html>'];
+                var fixture = '<html>\n<!-- build:js -->\nSome text\n<!-- endbuild -->\n</html>';
                 var expected = '<html>\nSome text\n</html>';
 
                 var stream = plugin({}, {keepUnassigned: true});
-                compare(es.readArray(fixture), expected, stream, done);
+                compare(stringToStream(fixture), expected, stream, done);
             });
 
             it('Should remove empty blocks', function (done) {
-                var fixture = ['<html>\n<!-- build:js -->\nSome text\n<!-- endbuild -->\n</html>'];
+                var fixture = '<html>\n<!-- build:js -->\nSome text\n<!-- endbuild -->\n</html>';
                 var expected = '<html>\n</html>';
 
                 var stream = plugin();
-                compare(es.readArray(fixture), expected, stream, done);
+                compare(stringToStream(fixture), expected, stream, done);
             });
         });
 
         describe('keepBlockTags', function () {
             it('Should keep placeholder tags without arguments', function (done) {
-                var fixture = ['<html>\n<!-- build:js -->\nSome text\n<!-- endbuild -->\n</html>'];
+                var fixture = '<html>\n<!-- build:js -->\nSome text\n<!-- endbuild -->\n</html>';
                 var expected = '<html>\n<!-- build:js -->\n<!-- endbuild -->\n</html>';
 
                 var stream = plugin({}, {keepBlockTags: true});
-                compare(es.readArray(fixture), expected, stream, done);
+                compare(stringToStream(fixture), expected, stream, done);
             });
 
             it('Should keep placeholder tags with arguments', function (done) {
-                var fixture = ['<html>\n<!-- build:lorem -->\nSome text\n<!-- endbuild -->\n</html>'];
+                var fixture = '<html>\n<!-- build:lorem -->\nSome text\n<!-- endbuild -->\n</html>';
                 var expected = '<html>\n<!-- build:lorem -->\nipsum\n<!-- endbuild -->\n</html>';
 
                 var stream = plugin({lorem: 'ipsum'}, {keepBlockTags: true});
-                compare(es.readArray(fixture), expected, stream, done);
+                compare(stringToStream(fixture), expected, stream, done);
             });
 
             it('Should remove placeholder tags without arguments', function (done) {
-                var fixture = ['<html>\n<!-- build:js -->\nSome text\n<!-- endbuild -->\n</html>'];
+                var fixture = '<html>\n<!-- build:js -->\nSome text\n<!-- endbuild -->\n</html>';
                 var expected = '<html>\n</html>';
 
                 var stream = plugin();
-                compare(es.readArray(fixture), expected, stream, done);
+                compare(stringToStream(fixture), expected, stream, done);
             });
 
             it('Should remove placeholder tags with arguments', function (done) {
-                var fixture = ['<html>\n<!-- build:lorem -->\nSome text\n<!-- endbuild -->\n</html>'];
+                var fixture = '<html>\n<!-- build:lorem -->\nSome text\n<!-- endbuild -->\n</html>';
                 var expected = '<html>\nipsum\n</html>';
 
                 var stream = plugin({lorem: 'ipsum'});
-                compare(es.readArray(fixture), expected, stream, done);
+                compare(stringToStream(fixture), expected, stream, done);
             });
         });
 
         describe('resolvePaths', function () {
             it('Should resolve relative paths', function (done) {
-                var fixture = ['<html>\n<!-- build:js -->\n<script src="file.js"></script>\n<!-- endbuild -->\n</html>'];
+                var fixture = '<html>\n<!-- build:js -->\n<script src="file.js"></script>\n<!-- endbuild -->\n</html>';
                 var expected = '<html>\n<script src="../lib/script.js"></script>\n</html>';
 
                 var stream = plugin({js: 'lib/script.js'}, {resolvePaths: true});
-                compare(es.readArray(fixture), expected, stream, done);
+                compare(stringToStream(fixture), expected, stream, done);
             });
         });
     });
 
     describe('Legacy versions', function () {
         it('[version <1.2] should keep empty blocks (keepUnused = true)', function (done) {
-            var fixture = ['<html>\n<!-- build:js -->\nThis should not be removed\n<!-- endbuild -->\n</html>'];
+            var fixture = '<html>\n<!-- build:js -->\nThis should not be removed\n<!-- endbuild -->\n</html>';
             var expected = '<html>\nThis should not be removed\n</html>';
 
             var stream = plugin({}, true);
-            compare(es.readArray(fixture), expected, stream, done);
+            compare(stringToStream(fixture), expected, stream, done);
         });
 
         it('[version <1.2] should remove empty blocks (keepUnused = false)', function (done) {
-            var fixture = ['<html>\n<!-- build:js -->\nThis should be removed\n<!-- endbuild -->\n</html>'];
+            var fixture = '<html>\n<!-- build:js -->\nThis should be removed\n<!-- endbuild -->\n</html>';
             var expected = '<html>\n</html>';
 
             var stream = plugin();
-            compare(es.readArray(fixture), expected, stream, done);
+            compare(stringToStream(fixture), expected, stream, done);
         });
     });
 
